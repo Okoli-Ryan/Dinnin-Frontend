0b4edb7391223dfa6aa632a125bcaeefdc88752e
okoli-ryan <okoliryan50@gmail.com>
2023-02-26 15:59:33 +0100

    remove checkout list dummt

--- src/features/Checkout/components/checkoutList/CheckoutList.dummy.ts
+++
@@ -1,4 +0,0 @@
-import { Dummy__CartList } from '../../../Cart/components/cartList/CartList.dummy';
-import { ParseCheckoutList } from './utils';
-
-export const Dummy_CheckoutList = ParseCheckoutList(Dummy__CartList);
