bcd07650b517868b94c9421611fd2a7b9ccf3b72
okoli-ryan <okoliryan50@gmail.com>
2023-02-26 16:03:59 +0100

    removed empty files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { PersistGate } from "redux-persist/lib/integration/react";
 
 import { getPersistor } from "@rematch/persist";
 
-import RoutesConfig from "./features/Routes/Routes";
+import RoutesConfig from "./features/Routes";
 
 export default function App() {
 	const persistor = getPersistor();
