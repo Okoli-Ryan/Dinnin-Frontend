752d12e0763fc9379cb70f96ed101c54e0758ebe
okoli-ryan <okoliryan50@gmail.com>
2023-02-28 11:26:10 +0100

    dynamic imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import mkcert from "vite-plugin-mkcert";
 
 import react from "@vitejs/plugin-react";
 
-const plugins = process.env.ENV === "production" ? [react()] : [react(), mkcert()];
-
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins,
+	plugins: [react(), mkcert()],
 	server: { https: true },
 });
