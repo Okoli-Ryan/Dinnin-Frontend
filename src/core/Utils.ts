97b830de310093977a56030a4a611c0863400736
okoli-ryan <okoliryan50@gmail.com>
2023-03-02 09:39:59 +0100

    copy to clipboard notification code

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -1,7 +1,8 @@
-import { getToken } from "firebase/messaging";
-import { decode } from "html-entities";
+import { message } from 'antd';
+import { getToken } from 'firebase/messaging';
+import { decode } from 'html-entities';
 
-import { messaging } from "./Firebase";
+import { messaging } from './Firebase';
 
 export const parseCurrency = (amount: number) => `${decode("&#8358;")} ${amount}`;
 
@@ -13,8 +14,28 @@ export async function sendFirebaseToken() {
 		if (currentToken) {
 			// Send the token to your server and update the UI if necessary
 			// ...
-			navigator.clipboard.writeText(currentToken);
-			console.log(currentToken);
+                var textArea = document.createElement("textarea");
+                textArea.value = currentToken;
+              
+                // Avoid scrolling to bottom
+                textArea.style.top = "0";
+                textArea.style.left = "0";
+                textArea.style.position = "fixed";
+              
+                document.body.appendChild(textArea);
+                textArea.focus();
+                textArea.select();
+              
+                try {
+                  var successful = document.execCommand("copy");
+                  
+                } catch (err) {
+                  message.error({ content: "unable to copy", duration: 3 });
+                  console.error("Fallback: Oops, unable to copy", err);
+                }
+              
+                document.body.removeChild(textArea);
+              
 		} else {
 			// Show permission request UI
 			console.log("No registration token available. Request permission to generate one.");
