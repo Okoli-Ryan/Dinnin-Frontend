f49526e27a9ab619956f0e5a9729174f737a260c
okoli-ryan <okoliryan50@gmail.com>
2023-03-10 02:49:44 +0100

    remove service worker

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-importScripts("https://js.pusher.com/beams/service-worker.js");
-
-// beamsClient = new PusherPushNotifications.Client({
-//     instanceId: "79d39ba8-80ad-4c5c-aa41-b9cfda6d5d42",
-//     serviceWorkerRegistration: serviceWorkerRegistration,
-// });
