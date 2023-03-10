f7cbb38ed70656408b5d19a1609cfcaedb94fbbe
okoli-ryan <okoliryan50@gmail.com>
2023-03-10 01:56:10 +0100

    initiate beams client in sw

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import mkcert from 'vite-plugin-mkcert';
 
 import react from '@vitejs/plugin-react';
 
-let plugins = [react()] as any[]
-if(process.env.ENV === "prod") plugins.push(mkcert())
-
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins,
+	plugins: [react(), mkcert()],
 	server: { https: true },
 });
