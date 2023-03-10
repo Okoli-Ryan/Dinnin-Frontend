00dcc31b47c160d0cf787ac0479d57f0205fd0fd
okoli-ryan <okoliryan50@gmail.com>
2023-03-10 01:36:16 +0100

    remove cert from prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 //@ts-ignore
-import mkcert from "vite-plugin-mkcert";
+import mkcert from 'vite-plugin-mkcert';
 
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+
+let plugins = [react()] as any[]
+if(process.env.ENV === "prod") plugins.push(mkcert())
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), mkcert()],
+	plugins,
 	server: { https: true },
 });
