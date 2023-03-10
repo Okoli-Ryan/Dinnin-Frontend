5e1d03bf21a1f463c9532930057bc97f93b14174
okoli-ryan <okoliryan50@gmail.com>
2023-03-10 02:10:59 +0100

    beams sw

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
 importScripts("https://js.pusher.com/beams/service-worker.js");
 
-// window.navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
-const beamsClient = new PusherPushNotifications.Client({
-	instanceId: "79d39ba8-80ad-4c5c-aa41-b9cfda6d5d42",
-	// serviceWorkerRegistration: serviceWorkerRegistration,
+window.navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+	const beamsClient = new PusherPushNotifications.Client({
+		instanceId: "79d39ba8-80ad-4c5c-aa41-b9cfda6d5d42",
+		serviceWorkerRegistration: serviceWorkerRegistration,
+	});
 });
-// });
